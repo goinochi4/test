8a76a6557f6ad245200eb5d8f8ecaf7e01ab0c0e
goinochi4 <106500661+goinochi4@users.noreply.github.com>
2022-06-03 14:11:06 +0900

    大会一覧のところ、<ul><li>タグから<table>タグへ移行。

--- date.php
+++ date.php
@@ -1,6 +1,6 @@
 <?php
 
-//大会名、その日程のデータを表示するためのクラス。（※実際の運用ではデータベースに接続するのでこのようなクラスは使わない。）
+//大会名、その日程のデータを表示するためのクラス。（※実際の運用ではデータベースに接続するのでこのようなクラスによる管理は行わない。）
 class Tounament{
    function __construct($tounamentDistrict,$tounamentDate){
        $this->tounamentDistrict = $tounamentDistrict;
@@ -34,8 +34,8 @@ class Tounament{
     $tounament1 = new Tounament('札幌','2022-05-04');
     $tounament2 = new Tounament('仙台','2022-06-05');
     $tounament3 = new Tounament('東京','2022-07-12');
-    $tounament4 = new Tounament('名古屋','2022-8-02');
-    $tounament5 = new Tounament('大阪','2022-9-21');
+    $tounament4 = new Tounament('名古屋','2022-08-02');
+    $tounament5 = new Tounament('大阪','2022-09-21');
     $tounament6 = new Tounament('広島','2022-10-13');
     $tounament7 = new Tounament('福岡','2022-11-08');
     $tounament8 = new Tounament('那覇','2022-12-15');
@@ -49,20 +49,20 @@ class Tounament{
     <head>
         <meta charset="utf-8">
         <title>申し込みフォーム</title>
+        <link rel="stylesheet" href="date.css">
     </head>
     <body>
 
-    <h1 style="font-size:22px; font-weight: normal;">（仮）素人囲碁クラス別日本一決定戦&nbsp;&nbsp;申し込みページ</h1>
-        <h2 style="font-size:19px; font-weight: normal; font-size:20px;">・大会一覧（半年以内に行われるもの）</h2>
-        <div class="apply_select_place">
-           <ul>
-              <?php foreach($tounaments1 as $tounament1):?>
-               
-               <?php if($tounament1->getInvertBetTodayToTouna() === 1 && $tounament1->getDifferBetTodayToTouna() >= 0 && $tounament1->getDifferBetTodayToTouna() <= 60){ echo '<li style="list-style-type: none;">'.$tounament1->getTounamentDistrict().'大会（受付は終了いたしました。）['.$tounament1->getTounamentDate().']</li>';} else if( $tounament1->getInvertBetTodayToTouna() === 0 && $tounament1->getDifferBetTodayToTouna() > 0 && $tounament1->getDifferBetTodayToTouna() < 90){ echo '<li style="list-style-type: none;"><a href="#">'.$tounament1->getTounamentDistrict().'大会</a>（受付中）&nbsp;&nbsp;['.$tounament1->getTounamentDate().']</li>';} else if($tounament1->getDifferBetTodayToTouna() >= 90 && $tounament1->getDifferBetTodayToTouna() < 180 ){ echo '<li style="list-style-type: none;">'.$tounament1->getTounamentDistrict().'大会（準備中です。）['.$tounament1->getTounamentDate().']</li>';}else{ continue; } ?> 
+       <h1>（仮）素人囲碁クラス別日本一決定戦&nbsp;&nbsp;申し込みページ</h1>
+        <h2>大会一覧（半年以内に行われるもの）</h2>
+           <div class="apply_select_place">
+            <table>
+              <?php foreach($tounaments1 as $tounament1):?> 
+               <?php if($tounament1->getInvertBetTodayToTouna() === 1 && $tounament1->getDifferBetTodayToTouna() >= 0 && $tounament1->getDifferBetTodayToTouna() <= 60){ echo '<tr><td>'.$tounament1->getTounamentDistrict().'大会</td><td>['.$tounament1->getTounamentDate().']</td><td>（受付は終了しました。）</td></tr>';} else if( $tounament1->getInvertBetTodayToTouna() === 0 && $tounament1->getDifferBetTodayToTouna() > 0 && $tounament1->getDifferBetTodayToTouna() < 90){ echo '<tr><td><a href="#">'.$tounament1->getTounamentDistrict().'大会</a></td><td>['.$tounament1->getTounamentDate().']</td><td>（受付中）</td></tr>';} else if($tounament1->getDifferBetTodayToTouna() >= 90 && $tounament1->getDifferBetTodayToTouna() < 180 ){ echo '<tr><td>'.$tounament1->getTounamentDistrict().'大会</td><td>['.$tounament1->getTounamentDate().']</td><td>（準備中です。）</td></tr>';;}else{ continue; } ?> 
                <?php endforeach;?>
-           </ul>　 
-            <!-- 上のコードでやっていることは...１、過去の大会でかつ６０日過ぎたものは表示しない。２、過去の大会で６０日以内のものは「受付終了」としリンクを外したうえで表示。３，大会当日から９０日以内は「受付中」としてリンクをオンにして表示。４，大会まで９０日以上１８０日以内のものは「準備中」としてリンクを外して表示。５，大会まで９０日以上のものは表示しない。-->
-        </div>
+           　</table>
+            <!-- 上のコードでやっていることは...１、過去の大会でかつ６０日過ぎたものは表示しない。２、過去の大会で６０日以内のものは「受付終了」としリンクを外したうえで表示。３，大会当日から９０日以内は「受付中」としてリンクをオンにして表示。４，９０日以上先及び１８０日以内に行われる大会は「準備中」としてリンクを外して表示。５，１８０日以上先の大会は表示しない。-->
+           </div>
         
      </body>
     </html>
